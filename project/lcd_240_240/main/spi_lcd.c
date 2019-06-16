e3fbd011edba0fa10c86054063a3e52d60dd60be
xiongyu <xiongyumail@gmail.com>
2019-06-16 17:26:51 +0200

    flash test ok

--- project/lcd_240_240/main/spi_lcd.c
+++ project/lcd_240_240/main/spi_lcd.c
@@ -127,22 +127,6 @@ static void initialise_wifi(void)
     ESP_ERROR_CHECK( esp_wifi_start() );
 }
 
-int font_generate_bin(const char *filename, const lv_font_t *font)
-{
-    int ret;
-    size_t size;
-
-    size = font->glyph_dsc[font->glyph_cnt - 1].glyph_index + font->glyph_dsc[font->glyph_cnt - 1].w_px * font->h_px;
-
-    FILE *fp=fopen(filename, "w");
-    if (fp == NULL) {
-        return -1;
-    }
-    ret = fwrite(font->glyph_bitmap, sizeof(uint8_t), size, fp);
-    fclose(fp);
-    return ret;
-}
-
 void app_main()
 {
     ESP_LOGI(TAG, "[APP] Startup..");
@@ -162,41 +146,6 @@ void app_main()
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
-
-    ESP_LOGI(TAG, "Initializing SPIFFS");
-    
-    esp_vfs_spiffs_conf_t conf = {
-      .base_path = "/spiffs",
-      .partition_label = NULL,
-      .max_files = 5,
-      .format_if_mount_failed = true
-    };
-    
-    // Use settings defined above to initialize and mount SPIFFS filesystem.
-    // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
-    ret = esp_vfs_spiffs_register(&conf);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
-        }
-        return;
-    }
-    
-    // size_t total = 0, used = 0;
-    // ret = esp_spiffs_info(NULL, &total, &used);
-    // if (ret != ESP_OK) {
-    //     ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
-    // } else {
-    //     ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-    // }
-
-    LV_FONT_DECLARE(seg_font);
-    printf("seg_font.bin generate: %d\n", font_generate_bin("/spiffs/font_seg.bin", &seg_font));
 
     ESP_LOGI(TAG, "Initializing SNTP");
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
